e8b9bffc993966562c18a437d3433a25b40d85a8
Kamil <kjstepaniak1@gmail.com>
2020-09-11 23:36:41 +0100

    Updates

---
+++ precache-manifest.61b03ca2eaa4018a458e64928555f6b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "017ad1278f919a56824411d72e302fdd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2685781d4dd770329c0",
+    "url": "/static/css/main.f60d4025.chunk.css"
+  },
+  {
+    "revision": "687beae5eaa8f4f6488c",
+    "url": "/static/js/2.4aadfb67.chunk.js"
+  },
+  {
+    "revision": "3e8564503e698f586fbe21d8173263d8",
+    "url": "/static/js/2.4aadfb67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2685781d4dd770329c0",
+    "url": "/static/js/main.cbf53acd.chunk.js"
+  },
+  {
+    "revision": "f294869ae1697dfe421d",
+    "url": "/static/js/runtime-main.7ecb0647.js"
+  }
+]);
